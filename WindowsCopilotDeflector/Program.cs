8105c9edd11488e22fda27cf7aac123bd7207e79
StarNumber12046 <64470722+StarNumber12046@users.noreply.github.com>
2023-11-02 09:24:55 +0100

    Add project files.

---
+++ WindowsCopilotDeflector/Program.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using System.Web;
+
+// The following code is taken from the EdgeDeflector repo
+static bool IsUri(string uristring)
+{
+    try
+    {
+        Uri uri = new Uri(uristring);
+        return true;
+    }
+    catch (UriFormatException)
+    {
+    }
+    catch (ArgumentNullException)
+    {
+    }
+    return false;
+}
+
+static bool IsHttpUri(string uri)
+{
+    return uri.StartsWith("HTTPS://", StringComparison.OrdinalIgnoreCase) || uri.StartsWith("HTTP://", StringComparison.OrdinalIgnoreCase);
+}
+
+static bool IsMsEdgeUri(string uri)
+{
+    return uri.StartsWith("MICROSOFT-EDGE:", StringComparison.OrdinalIgnoreCase) && !uri.Contains(" ");
+}
+
+static bool IsNonAuthoritativeWithUrlQueryParameter(string uri)
+{
+    return uri.Contains("microsoft-edge:?") && uri.Contains("&url=");
+}
+
+static string GetURIFromCortanaLink(string uri)
+{
+    NameValueCollection queryCollection = HttpUtility.ParseQueryString(uri);
+    return queryCollection["url"];
+}
+
+static string RewriteMsEdgeUriSchema(string uri)
+{
+    string msedge_protocol_pattern = "^microsoft-edge:/*";
+
+    Regex rgx = new Regex(msedge_protocol_pattern);
+    string new_uri = rgx.Replace(uri, string.Empty);
+
+    if (IsHttpUri(new_uri))
+    {
+        return new_uri;
+    }
+
+    // May be new-style Cortana URI - try and split out
+    if (IsNonAuthoritativeWithUrlQueryParameter(uri))
+    {
+        string cortanaUri = GetURIFromCortanaLink(uri);
+        if (IsHttpUri(cortanaUri))
+        {
+            // Correctly form the new URI before returning
+            return cortanaUri;
+        }
+    }
+
+    // defer fallback to web browser
+    return "http://" + new_uri;
+}
+
+static void OpenUri(string uri)
+{
+    if (!IsUri(uri) || !IsHttpUri(uri))
+    {
+        Environment.Exit(1);
+    }
+
+    ProcessStartInfo launcher = new ProcessStartInfo(uri)
+    {
+        UseShellExecute = true
+    };
+    Process.Start(launcher);
+}
+
+// The code following is by me :)
+
+Console.WriteLine("[*] The process has been launched with the following arguments: ");
+foreach (string arg in args)
+{
+    Console.WriteLine("[+] Argument: " + arg);
+}
+
+// Find if the program has been launched as a protocol
+
+bool bAsProtocol = false;
+
+foreach (string arg in args)
+{
+    if (arg.StartsWith("microsoft-edge://"))
+    {
+        Console.WriteLine("[*] We have been launched as a protocol");
+        bAsProtocol = true;
+    }
+}
+
+
+if (!bAsProtocol)
+{
+    Console.WriteLine("[+] The microsoft edge launch was \"intentional\". Launching original edge...");
+    ProcessStartInfo psi = new ProcessStartInfo();
+    psi.FileName = "original-msedge.exe";
+    psi.Arguments = String.Join(" ", args);
+    Process.Start(psi);
+//    Environment.Exit(0);
+}
+
+foreach(string arg in args)
+{
+    Console.WriteLine("[*] Processing argument " + arg);
+    Console.WriteLine("[*] IsMsEdgeUri(arg): " + IsMsEdgeUri(arg));
+    if (IsMsEdgeUri(arg))
+    {
+        Console.WriteLine("[+] RewriteMsEdgeUriSchema(arg): " + RewriteMsEdgeUriSchema(arg));
+        OpenUri(RewriteMsEdgeUriSchema(arg));
+//        Environment.Exit(0);
+    }
+}
+
+Console.ReadKey();
---
+++ WindowsCopilotDeflector/README.md
@@ -0,0 +1,21 @@
+﻿# Microsoft Edge Rewriter
+
+APP STATUS: Beta
+
+## What is this?
+This is a continuation of the now dead project EdgeDeflector
+
+## How does this work?
+This program is put in place of the original msedge executable (which is renamed to original-msedge.exe). If it detects that edge was launched by a protocol, it rewrites it (using the original EdgeDeflector code) and launches the standard browser. Otherwise, it launches edge.
+
+## How do I use this?
+For now, an installer is not provided (this is very beta). Just go to C:\Program Files (x86)\Microsoft\Edge\Application and rename msedge.exe to original-msedge.exe, build USING THE PUBLISH TEMPLATE this solution and put the generated exe in the folder with the name msedge.exe
+
+## It stopped working, why?
+Micro$oft probably forced your PC to reinstall edge. Follow again the installation instructions and make sure to use the updated edge as orginal-msedge.exe
+
+## What works? What doesn't?
+Pretty much everything that worked with edgedeflector should work, but I'm not too sure with Windows Copilot. If it does not work, I'l try my best to support it.
+
+## Other notes
+This project uses code from edgedeflector. Check out the original project if you want. https://github.com/da2x/EdgeDeflector
